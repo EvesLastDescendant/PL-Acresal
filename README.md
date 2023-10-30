aa160daa2d841e0bed801fb0cb60de6b5224dcc2
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2023-10-30 13:07:24 +0100

    removed unnewcessary files and folders, created and linked new pages, added pictures

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ All the files required are all here.
 ## Setup
 
 * got to [https://github.com/EvesLastDescendant/PL-Acresal]
-* on the menu bar holding the name of the repo, locate "Fork" and click.
+* on the menu bar holding the name of the repo, locate the "Fork" button and click.
 * follow the instructions to setup a copy of the repo in your github.
 * download the repo to your local machine
 * Done!
@@ -14,3 +14,6 @@ All the files required are all here.
 
 before working on the files, create a branch; that'll be your working branch
 add, commit, and update in your branch before merging with the main
+
+site is now live:
+[pl-acresal.netlify.app]
--- where_we_work/saveweb2zip-com-acresal-gov-ng (4)/js/portfolio.js
+++
@@ -1,9 +0,0 @@
-/*! 2.3.1
- *  All required plugins 
- *  http://averta.net/phlox/
- */
-/*! 
- * 
- * ================== public/assets/js/src/init.general.js =================== 
- **/
-(function($){var filterList=$(".aux-widget-recent-portfolios .aux-ajax-filters li[data-filter]");filterList.on("click",(function(e){e.preventDefault();var $this=$(this),$container=$this.parents(".aux-widget-recent-portfolios").find(".aux-ajax-view"),data={action:"aux_recent_portfolio_filter_content",term:$this.data("category-id"),taxonomy:$container.data("taxonomy"),num:$container.data("num"),order:$container.data("order"),orderby:$container.data("orderby"),n:$container.data("n"),args:eval($container.data("element-id")+"AjaxConfig")};$container.AuxIsotope("removeAll"),$container.AuxIsotope("showLoading"),$.post(auxpfo.ajax_url,data,(function(t){if(t){var a=$(t.data);setTimeout((function(){a.each((function(t,a){$item=$(a),$item.hasClass("aux-iso-item")&&($container.AuxIsotope("insert",$item),$item.AuxTwoWayHover(),$item.photoSwipe({target:".aux-lightbox-btn",bgOpacity:.97,shareEl:!0}))}))}),1e3)}}))}))})(jQuery);
